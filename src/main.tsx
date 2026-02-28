a9357b8517429ce16326046d04220616144df131
Kavya-P-1016 <83158328+Kavya-P-1016@users.noreply.github.com>
2026-02-28 13:57:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+No, but are you fucking kidding me? This is not what is in my main, it is completely different. Are you fucking kidding me?import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-)
+  </StrictMode>,
+);
